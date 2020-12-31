e716ec2f1dba99aa19ce0c5b1b6e6c3a24316535
fatalcenturion <tom.b.2k2@gmail.com>
2020-12-31 00:53:54 +0000

    modified REPL env for development of the encryption protocol

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::io::{Read, Stdin, Stdout, Stderr, stdin, stdout, stderr, Write};
+use std::io::{Read, Stdin, Stdout, Stderr, stdin, stdout, stderr, Write, BufReader, BufRead, BufWriter};
 use std::path::PathBuf;
+use std::process::exit;
+
+use orion::aead::*;
 use crossterm::cursor;
 use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::style::Print;
@@ -22,11 +25,48 @@ fn main() {
     let mut current_db = "None".to_string();
     // let mut location = std::env::current_dir().unwrap();
     let mut connection = TcpStream::connect("localhost:1234").unwrap();
+    let mut r = BufReader::new(&mut connection);
+    let mut is_key = true;
+    let mut raw_key = Vec::<u8>::new();
+    let mut key = SecretKey::default();
     loop {
-        let input = read_input(&current_db, &mut stdin, &mut stdout, &mut stderr, &input_history);
+        let mut byte: [u8; 1] = [0];
+        r.read_exact(&mut byte);
 
-        connection.write_all(format!("{}\n", input).as_bytes());
-        connection.flush();
+
+        if is_key {
+            let bt = byte.first().unwrap();
+            match bt {
+                10 => {
+                    is_key = false;
+                    // println!("key: {:?}", key);
+                    let sec = SecretKey::from_slice(raw_key.as_slice());
+                    if sec.is_ok() {
+                        println!("Key created correctly");
+                        key = sec.unwrap();
+                        println!("Key match: {}", key == raw_key.as_slice());
+                    } else {
+                        println!("{}", sec.unwrap_err());
+                    }
+                }
+                _ => {
+                    raw_key.push(bt.clone());
+                }
+            }
+        } else {
+            println!("Sending response");
+            let mut writer = BufWriter::new(&mut connection);
+            let payload = seal(&key, b"Hello World").unwrap();
+            writer.write_all(payload.as_slice());
+            writer.flush();
+            exit(1);
+        }
+
+
+        // let input = read_input(&current_db, &mut stdin, &mut stdout, &mut stderr, &input_history);
+        //
+        // connection.write_all(format!("{}\n", input).as_bytes());
+        // connection.flush();
 
         // input_history.push(input.clone());
         // current_db = commands::parse(input, &current_db);
