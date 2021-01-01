bd109ebd551d7d95b71499afce7059b97a7118b4
altrius <tom.b.2k2@gmail.com>
2021-01-01 20:37:01 +0000

    working on encryption API of CORE

--- src/structs.rs
+++ src/structs.rs
@@ -1,27 +1,51 @@
-pub const LEFT: [u8; 6] = *b"\x1b[0;75";
-pub const RIGHT: [u8; 6] = *b"\x1b[0;77";
-pub const UP: [u8; 4] = *b"\x1b[[A";
-pub const DOWN: [u8; 6] = *b"\x1b[0;80";
-pub const SEMICOLON: [u8; 1] = *b";";
-pub const ESCAPE: [u8; 1] = *b"\\";
-pub const NEWLINE: [u8; 1] = *b"\n";
+use std::io::{Read, Write, BufReader, BufWriter};
+use std::net::TcpStream;
+use std::process::exit;
 
-pub enum Character {
-    Left,
-    Right,
-    Up,
-    Down,
-    SemiColon,
-    Escape,
-    Shift,
-    Other,
+use orion::aead::*;
+
+pub struct Connection {
+    stream: TcpStream,
 }
 
-pub fn char_from_byte(byte: &[u8]) -> Character {
-    println!("bytes: {:?}", byte);
-    return match byte {
-        _ => {
-            Character::Other
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream
         }
-    };
+    }
+
+    pub fn read_ley(&mut self) -> SecretKey {
+        let mut r = BufReader::new(&mut self.stream);
+        let mut bytes: [u8; 72] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        let res = r.read_exact(&mut bytes);
+        let mut key = SecretKey::default();
+
+        if res.is_ok() {
+            let sec = SecretKey::from_slice(&bytes);
+            if sec.is_ok() {
+                println!("Key created correctly");
+                key = sec.unwrap();
+            } else {
+                println!("{}", sec.unwrap_err());
+            }
+        } else {
+            println!("\x1b[31mAn error occured during key transfer. Exiting...\x1b[0m");
+            exit(1);
+            // println!("READ ERR: {}", res.unwrap_err());
+        }
+
+        return key;
+    }
+
+    pub fn write_all(&mut self, key: &SecretKey, content: &str) {
+        let mut writer = BufWriter::new(&mut self.stream);
+        let res = seal(key, content.as_bytes());
+        if res.is_ok() {
+            writer.write_all(res.unwrap().as_slice());
+            writer.flush();
+        } else {
+            println!("Encountered an encryption error:\n{}", res.unwrap_err());
+        }
+    }
 }
